e5e233d8be219b1f198af71c7c9da6c43436c940
Guillermovalerianodj <guillermo.valeriano@iestpdj.edu.pe>
2020-11-19 22:27:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-nxnsp2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-nxnsp2)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+let arreglo = ["ruby", "c", "python", "javascript"];
+arreglo.forEach(function(elemento) {
+  console.log(elemento);
+  alert(elemento);
+});
